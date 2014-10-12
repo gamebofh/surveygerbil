00b1d6d1f924a9be8f7d4a6fa7e0f3a51e0de5d8
Jot Powers <gamebofh@bitbucket.org>
2014-10-11 19:53:53 -0700

    new test

---
+++ grab.py
@@ -0,0 +1,12 @@
+import mechanize
+BASE_URL = "https://www.surveymonkey.com/s/KLCBNM5"
+br = mechanize.Browser()
+data = br.open(BASE_URL).get_data()
+links = scrape_links(BASE_URL, data)
+
+for link in links:
+data = br.follow_link(link).get_data()
+scrape_articles(data)
+br.back()
+
+print ("data")
