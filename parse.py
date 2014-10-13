b8ec2ca97777e29091ed6b24fb3ecdb537eb60d3
Jot Powers <gamebofh@bitbucket.org>
2014-10-13 10:08:00 -0700

    fixing to work for me

--- parse.py
+++ parse.py
@@ -1,5 +1,7 @@
 # Needs to read in mech.out
 # Then we'll parse
 # http://axialcorps.com/2013/09/27/dont-slurp-how-to-read-files-in-python/
-f = open('~/git/surveygerbal/mech.out', 'r+')
+f = open('mech.out', 'r+')
 contents = f.read()
+
+print (contents)
