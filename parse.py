753cde7633b1eee2197a91517c570730fd1b6217
destrodox <destrodox@bofh.com>
2014-10-13 10:02:39 -0700

    Signed-off-by: destrodox <destrodox@bofh.com>

--- parse.py
+++ parse.py
@@ -1,2 +1,5 @@
 # Needs to read in mech.out
 # Then we'll parse
+# http://axialcorps.com/2013/09/27/dont-slurp-how-to-read-files-in-python/
+f = open('~/git/surveygerbal/mech.out', 'r+')
+contents = f.read()
