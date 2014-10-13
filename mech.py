357ea2e2d7ff07726a6031fde7631d3b65ab6561
Jot Powers <gamebofh@bitbucket.org>
2014-10-12 20:24:11 -0700

    working

--- mech.py
+++ mech.py
@@ -1,12 +1,16 @@
 import mechanize
 BASE_URL = "https://www.surveymonkey.com/s/KLCBNM5"
-br = mechanize.Browser()
-data = br.open(BASE_URL).get_data()
-links = scrape_links(BASE_URL, data)
+response = mechanize.urlopen(BASE_URL)
+print response.read()
 
-for link in links:
-    data = br.follow_link(link).get_data()
-scrape_articles(data)
-br.back()
+#br = mechanize.Browser()
+#data = br.open(BASE_URL).get_data()
+#print br.read()
+#links = scrape_links(BASE_URL, data)
 
-print ("data")
+#for link in links:
+#    data = br.follow_link(link).get_data()
+#scrape_articles(data)
+#br.back()
+
+#print ("data")
