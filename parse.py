82256ec90e00431bf8b69f25e5254824eab61216
destrodox <destrodox@bofh.com>
2014-10-13 10:46:06 -0700

    Signed-off-by: destrodox <destrodox@bofh.com>

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 # Needs to read in mech.out
 # Then we'll parse
-
+# http://www.crummy.com/software/BeautifulSoup/bs4/doc/
 # http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
 from bs4 import BeautifulSoup
 
@@ -8,13 +8,13 @@ f = open('mech.out', 'r+')
 
 contents = f.read()
 
-print (contents)
+#print (contents)
 
-soup = BeautifulSoup(open("index.html"))
+soup = BeautifulSoup(open("mech.out"))
 
 soup = BeautifulSoup("<html>data</html>")
 
-
+print (soup)
 
 # Moving to bottom so we have it, but don't need it right now
 # http://axialcorps.com/2013/09/27/dont-slurp-how-to-read-files-in-python/
