04325a24b80dfae02a65d0e11edc8111026a4532
Jot Powers <gamebofh@bitbucket.org>
2014-10-13 10:17:56 -0700

    new stuff

--- parse.py
+++ parse.py
@@ -1,7 +1,14 @@
 # Needs to read in mech.out
 # Then we'll parse
-# http://axialcorps.com/2013/09/27/dont-slurp-how-to-read-files-in-python/
+
+# http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
 f = open('mech.out', 'r+')
 contents = f.read()
 
 print (contents)
+
+
+
+
+# Moving to bottom so we have it, but don't need it right now
+# http://axialcorps.com/2013/09/27/dont-slurp-how-to-read-files-in-python/
